7d640db8317dfc73d3821870ea3ebe3a5e472e6a
なまけもの <68767710+namakemono-san@users.noreply.github.com>
2025-03-10 09:30:54 +0900

    fix: docker gpg

--- scripts/setup_k8s.sh
+++ scripts/setup_k8s.sh
@@ -25,6 +25,8 @@ esac
 KUBE_API_SERVER_VIP="192.168.0.11"
 NODE_IP="$2"
 
+apt update && apt install -y apt-transport-https ca-certificates curl gnupg software-properties-common
+
 # Install Containerd
 cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
 overlay
@@ -44,18 +46,10 @@ EOF
 # Apply sysctl params without reboot
 sudo sysctl --system
 
-# Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-
 # Add the repository to Apt sources:
-echo \
-    "deb [arch=\$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-    \$(. /etc/os-release && echo "\$VERSION_CODENAME") stable" | \
-    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
+    | tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
 sudo apt-get install -y containerd.io
